d86dad2c54eaa4dd2427d287f857908af51da026
yinjiacheng <yinjiacheng@yidian-inc.com>
2021-03-31 18:59:06 +0800

    code init

---
+++ app/src/main/java/com/yjc/jetpacklearn/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yjc.jetpacklearn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
